36d644ecd6b7d5045352dc169ca5eec18e3b77d1
cdsleaf <cdsleaf@gmail.com>
2019-11-01 20:18:48 +1300

    refactor: adapt useReducer into LikeDislikeTest component

--- src/components/LikeDislikeTest.js
+++ src/components/LikeDislikeTest.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import styled from "styled-components";
 
 const ButtonContainer = styled.div`
@@ -11,40 +11,54 @@ const Button = styled.button`
   color: ${props => props.selected ? "blue" : "black"};
 `;
 
+const initialState = {
+  like: 100,
+  dislike: 100,
+  clickedButton: null,
+};
+
+const LIKE = "like";
+const DISLIKE = "dislike";
+
+const reducer = (state, action) => {
+  switch(action.type){
+    case LIKE:
+      return { 
+        like: state.clickedButton !== LIKE 
+          ? state.like+1 
+          : state.clickedButton === LIKE ? state.like-1 : state.like,
+        dislike: state.clickedButton === DISLIKE ? state.dislike-1 : state.dislike,
+        clickedButton: state.clickedButton === LIKE ? null : LIKE,
+      }
+    case DISLIKE:
+      return {
+        like: state.clickedButton === LIKE ? state.like-1 : state.like,
+        dislike: state.clickedButton !== DISLIKE 
+          ? state.dislike+1 
+          : state.clickedButton === DISLIKE ? state.dislike-1 : state.dislike,
+        clickedButton: state.clickedButton === DISLIKE ? null : DISLIKE,
+      }
+    default:
+      throw new Error();
+  }    
+}
+
 function LikeDislikeTest(props) {
 
-  const LIKE = "like";
-  const DISLIKE = "dislike";
-
-  const [like, setLike] = useState(100);
-  const [dislike, setDislike] = useState(100);
-  const [clickedButton, setClickedButton] = useState(null);
-
-  const handleClick = ({ target }) => {
-    const { id } = target;
-    
-    setLike(id === LIKE && clickedButton !== id 
-      ? like+1 
-      : clickedButton === LIKE ? like-1 : like
-    )
-    setDislike(id === DISLIKE && clickedButton !== id 
-      ? dislike+1 
-      : clickedButton === DISLIKE ? dislike-1 : dislike
-    )
-    setClickedButton(clickedButton === id ? null : id);
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const handleClick = ({ target: { id } }) => dispatch({type: id});
 
   return (
     <ButtonContainer>
       <Button 
-        selected={clickedButton === LIKE ? true : false} 
+        selected={state.clickedButton === LIKE ? true : false} 
         id={LIKE} 
-        onClick={handleClick}>Like: {like}
+        onClick={handleClick}>Like: {state.like}
       </Button>
       <Button 
-        selected={clickedButton === DISLIKE ? true : false} 
+        selected={state.clickedButton === DISLIKE ? true : false} 
         id={DISLIKE} 
-        onClick={handleClick}>Dislike: {dislike}
+        onClick={handleClick}>Dislike: {state.dislike}
       </Button>
     </ButtonContainer>
   );
